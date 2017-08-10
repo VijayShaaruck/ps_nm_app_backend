c1b1001ccf78677f9e8212c587880667fb339093
VijayShaaruck <vijayshaaruck96@gmail.com>
2017-08-10 15:23:11 +0530

    socket added

--- app.js
+++ app.js
@@ -29,12 +29,42 @@ server.route({
         var payload = request.payload
         connection.query("INSERT INTO address(name,mac,valid) VALUES('" + payload.name + "','" + payload.mac + "',0)", function(error, results, fields) {
             if (error) throw error;
-            //console.log('The solution is: ', results[0].solution);
+            emit("refresh")
         });
 
         return reply(payload);
     }
 });
+server.route({
+    method: 'GET',
+    path: '/send',
+    handler: function(request, reply) {
+        emit("DFBDSJKFNDSKL");
+        return reply("sned");
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/load',
+    handler: function(request, reply) {
+        connection.query('SELECT * FROM address', function(error, results, fields) {
+            if (error) throw error;
+            console.log('The solution is: ', results[0].name);
+            return reply(JSON.stringify(results));
+        });
+    }
+});
+
+var io = require("socket.io")(server.listener);
+
+io.on("connection", function(socket) {
+    // emit();
+    console.log('connected');
+})
+
+function emit(name) {
+    io.sockets.emit('address', { hello: name });
+}
 
 // Start the server
 server.start((err) => {
